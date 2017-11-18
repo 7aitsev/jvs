b33a988a3a1f41b85ec31c4538966581ac2b3883
Maxim Zaitsev <7aitsev@pm.me>
2017-11-18 11:38:30 +0300

    hotfixes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-OUTDIR=out
 
-git clone $1
-dir=$(ls)
-mkdir "$dir"/"$OUTDIR" && cd "$dir"/"$OUTDIR"
+git clone $1 && dir=$(ls) && cd "$dir"
 
-javac -sourcepath ../src/ ../src/Main.java -cp ../lib/gson-2.8.2.jar -d .
-java -classpath ../lib/gson-2.8.2.jar:.:service Main -Djava.util.logging.config.file=logging.properties
+javac -sourcepath src/ src/Main.java -cp lib/gson-2.8.2.jar -d .
+java -classpath lib/gson-2.8.2.jar:.:service -Djava.util.logging.config.file=logging.properties Main
