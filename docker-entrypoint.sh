aacf068eb6d9a6b3b6017f001602d4273db05143
Maksim Zaitsev <froxton@outlook.com>
2017-12-17 07:15:36 +0300

    removed .sh file

--- docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-git clone $1 && dir=$(ls) && cd "$dir"
-
-javac -sourcepath src/ src/Main.java -cp lib/gson-2.8.2.jar -d .
-java -classpath lib/gson-2.8.2.jar:.:service -Djava.util.logging.config.file=logging.properties Main
