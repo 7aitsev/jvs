b5780bf6900a743f2f688832845ed481d5652935
Maksim Zaitsev <froxton@outlook.com>
2017-12-21 01:45:42 +0300

    fixed #1: removed .jar from the repo

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,5 +2,7 @@
 
 git clone $1 && dir=$(ls) && cd "$dir"
 
-javac -sourcepath src/ src/Main.java -cp lib/gson-2.8.2.jar -d .
+mkdir lib/ && \
+wget --quiet -P lib/ "http://central.maven.org/maven2/com/google/code/gson/gson/2.8.2/gson-2.8.2.jar" && \
+javac -sourcepath src/ src/Main.java -cp lib/gson-2.8.2.jar -d . && \
 java -classpath lib/gson-2.8.2.jar:.:service -Djava.util.logging.config.file=logging.properties Main
